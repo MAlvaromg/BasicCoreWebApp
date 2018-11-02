a07c876c7a9ccb0a0053e44d5851889ff45231ca
Alvaro Marcos Gonzalez <alvaromg@microsoft.com>
2018-11-02 13:39:56 +0100

    Made student properties set private

--- BasicCoreWebApp/Domain/Student.cs
+++ BasicCoreWebApp/Domain/Student.cs
@@ -10,9 +10,9 @@ namespace BasicCoreWebApp.Domain
         public const int NameMaxLength = 60;
         public const int MinAge = 5;
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
 
         //There is no public constructor
         private Student() { }
