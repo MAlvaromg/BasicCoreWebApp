118e11b81ef3feac7a57ad27eb27629be0b931ff
Alvaro Marcos Gonzalez <alvaromg@microsoft.com>
2018-11-29 18:20:38 +0100

    Included Domain tests

---
+++ TestProject/Domain/StudentTests.cs
@@ -0,0 +1,40 @@
+﻿using BasicCoreWebApp.Domain;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestProject.Domain
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void When_creating_a_student_with_all_needed_parameters_Should_return_a_valid_student()
+        {
+            //arrange
+            var age = 15;
+            var name = "Test";
+
+            //act
+            var student = Student.Create(name, age);
+
+            //assert
+            student.Name.Should().Be(name);
+            student.Age.Should().Be(age);
+        }
+
+        [TestMethod]
+        public void When_creating_a_student_with_empty_name_Should_return_an_exception()
+        {
+            //arrange
+            var age = 15;
+            var name = "";
+
+            //act
+            Action createStudent = () => Student.Create(name, age);
+
+            //assert
+            createStudent.Should().Throw<DomainException>();
+        }
+    }
+}
